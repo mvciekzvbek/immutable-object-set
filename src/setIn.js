d4be15a20d4ecd05cb424b17dc98bdd0facdab84
mvciekzvbek <maciej.jan.zabek@gmail.com>
2020-09-13 15:38:48 +0200

    tests

--- src/setIn.js
+++ src/setIn.js
@@ -1,14 +1,14 @@
 export const setIn = (path, value, object) => {
-  if (!path.length) {
+  if (!Array.isArray(path) && !path.length) {
     return value;
   }
   let copy = Object.assign({}, object);
-  const current = path[0];
+  const currentElement = path[0];
   if (path.length > 1) {
     const newArray = path.slice(1);
-    const innerObject = copy.hasOwnProperty(current) ? copy[current] : {};
+    const innerObject = copy.hasOwnProperty(currentElement) ? copy[currentElement] : {};
     value = setIn(newArray, value, innerObject)
   }
-  copy[current] = value;
+  copy[currentElement] = value;
   return copy;
 };
